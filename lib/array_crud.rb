306774077207469d1bf40458d90221f3140fa81f
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-25 18:51:53 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["a","b","c","d"]
-  array.first(""#{element})
+  array.first("#{element}")
 end
 
 def remove_element_from_end_of_array(array)
